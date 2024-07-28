06da03bfd21a1afbdefbe510c3d2796d66598cb0
HELOISA BENEDET MENDES <hbm22@inf.ufpr.br>
2024-07-28 18:39:26 -0300

    exercicios

--- 5_Semestre/Introdução à Computação Científica/otimizacao/plot.py
+++ 5_Semestre/Introdução à Computação Científica/otimizacao/plot.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
+import os
+from glob import glob
 
 def main():
     csv_dir = '/home/bcc/hbm22/Results/'
     csv_dir = os.path.abspath(csv_dir)
 
-    for csv in glob()
+    print(csv_dir)
+    for csv in glob(csv_dir):
+        print('a')
+        print(csv)
 
 if __name__=='__main__':
     main()
