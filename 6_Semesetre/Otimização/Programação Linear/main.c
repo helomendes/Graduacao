5003cf769fdd4271541dfdcfa6b0e7c10123ccd9
Heloisa <heloisabmendes@gmail.com>
2024-12-05 17:29:36 -0300

    trabalho de otimização

---
+++ 6_Semesetre/Otimização/Programação Linear/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define MAX BUFSIZ
+
+int main() {
+	char buf[MAX];
+	fgets(buf, MAX, stdin);
+	
+	return 1;
+
+	int n, m;
+	//le n e m
+	//tem jeitos melhores de alocar vetores e matrizes
+	float v[n];
+	float q[m][2];
+	float c[n][m];
+	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			// le c[i][j]
+		}
+	}
+
+	//encerrada a leitura de dados
+
+	return 0;
+}
